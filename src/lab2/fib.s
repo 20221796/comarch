537fbc62c95a84e6f1369b1f09717d154dff9f1e
20221796 <dorisjbssu@gmail.com>
2023-12-01 15:55:28 +0900

    update fib

--- src/lab2/fib.s
+++ src/lab2/fib.s
@@ -12,18 +12,18 @@ fib:
 
     ldur x0, [sp, #0]             // 인수 n을 x0로 로드
     subs xzr, x0, #0              // n == 0을 확인
-    b.ne l1                       // n != 0이면 l1로 점프
+    b.ne L1                       // n != 0이면 L1로 점프
     mov x0, xzr                   // 결과값 레지스터에 0을 저장
-    b l3                          // l3로 점프
+    b L3                          // L3로 점프
 
-l1:
+L1:
     ldur x0, [sp, #0]             // 인수 n을 x0로 로드
     subs xzr, x0, #1              // n == 1을 확인
-    b.ne l2                       // n != 1이면 l2로 점프
+    b.ne L2                       // n != 1이면 L2로 점프
     mov x0, #1                    // 결과값 레지스터에 1을 저장
-    b l3                          // l3로 점프
+    b L3                          // L3로 점프
 
-l2:
+L2:
     ldur x0, [sp, #0]             // 인수 n을 x0로 로드
     sub x0, x0, #1                // n을 감소시킴
     bl fib                        // fib(n - 1)을 호출
@@ -35,7 +35,7 @@ l2:
 
     add x0, x0, x19               // fib(n - 1) + fib(n - 2)를 계산하여 결과값 레지스터에 저장
 
-l3:
+L3:
     ldur x19, [sp, #16]           // x19를 복구
     ldur x30, [sp, #40]           // x30를 복구 (Link Register)
     ldur x29, [sp, #32]           // x29를 복구 (Frame Pointer)

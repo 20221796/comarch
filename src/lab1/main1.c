ba807753167704dc45aee436dcde614cc10e1108
20221796 <dorisjbssu@gmail.com>
2023-12-01 12:50:31 +0900

    main update

--- src/lab1/main1.c
+++ src/lab1/main1.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
+
 extern void max(long long int* shvar, long long int array[], long long int size);
 long long int shvar;
 
-main() {
+int main() {
     long long int array[5] = {2, 23, 42, 51, 5};
     max(&shvar, array, 5);
     printf("MAX = %lld\n", shvar);
+    return 0;
 }

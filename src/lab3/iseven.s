2c2d66b11b2e6e71aba3228bf2b69d284539d737
20221796 <dorisjbssu@gmail.com>
2023-12-03 16:22:40 +0900

    report complete

--- src/lab3/iseven.s
+++ src/lab3/iseven.s
@@ -5,14 +5,13 @@
 isEven:
     mov x1, x0
     mov x0, #0
-    mov x2, #0
 
 loop:
-    and x3, x1, #1
-    add x0, x0, x3
+    and x2, x1, #1
+    add x0, x0, x2
     lsr x1, x1, #1
     cbnz x1, loop
 
     and x0, x0, #1
     eor x0, x0, #1
-    ret
+    br x30

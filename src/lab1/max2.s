a62c4924c874a1408b6110e34a45d7d9bc399a81
20221796 <dorisjbssu@gmail.com>
2023-12-10 12:30:30 +0900

    max2 update

--- src/lab1/max2.s
+++ src/lab1/max2.s
@@ -3,51 +3,53 @@
 .type max, STT_FUNC
 
 max:
-	sub  sp, sp, #40
+	sub sp, sp, #56
 	stur x19, [sp, #0]
 	stur x20, [sp, #8]
 	stur x21, [sp, #16]
 	stur x22, [sp, #24]
 	stur x23, [sp, #32]
+	stur x24, [sp, #40]
+	stur x25, [sp, #48]
 
-	add x19, xzr, xzr
-	ldr x22, [x1, x19, LSL #3]
-	str x22, [x0]
+	mov x19, x0             
+	mov x20, x1             
+	mov x21, x2             
+	mov x22, x3             
+	mov x23, xzr            
 
-// Lock acquire
-acquire_lock:
-	ldxr x10, [x3]
-	cbnz x10, acquire_lock
-	mov x10, #1
-	stur x10, [x3]
+lock:
+	ldaxr x24, [x19]
+	cbnz x24, lock
+	mov x24, #1
+	stlxr w24, w24, [x19]
+	cbnz w24, lock //컴파일하면 r24 사용 불가라고 떠서 w24로 변경
 
 mwhile:
-	cmp x2, x19
-	b.le mexit
-	ldr x23, [x1, x19, LSL #3]
-	ldr x22, [x0]
-	cmp x23, x22
+	cmp x22, x23
+	b.le free
+	ldr x24, [x21, x23, LSL #3]
+	ldr x25, [x20]
+	cmp x24, x25
 	b.le continue
-
-	// Check lock before updating
-	ldxr x10, [x3]
-	cbnz x10, continue
-	str x23, [x0]
+	str x24, [x20]
 continue:
-	add x19, x19, #1
+	add x23, x23, #1
 	b mwhile
 
-mexit:
-	// Lock release
-	mov x10, #0
-	stur x10, [x3]
+free:
+	mov x24, xzr
+	stlr x24, [x19]
 
+cexit:
+	ldur x25, [sp, #48]
+	ldur x24, [sp, #40]
 	ldur x23, [sp, #32]
 	ldur x22, [sp, #24]
 	ldur x21, [sp, #16]
 	ldur x20, [sp, #8]
 	ldur x19, [sp, #0]
-	add sp, sp, #40
+	add sp, sp, #56
 	br x30
 
 .end
